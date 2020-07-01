7986b13dccefcb5da16b856cb0b5a6e1af9447d8
cristianvogel <cristian.vogel55@gmail.com>
2020-07-01 19:48:37 +0200

    Working on improving IP switching to be more auto-aware of socket change

---
+++ GlobSeqHelpers.cpp
@@ -0,0 +1,17 @@
+//
+//  GlobSeqHelpers.cpp
+//  All
+//
+//  Created by Cristian Andres Vogel on 01/07/2020.
+//
+
+#include <iostream>
+#include "GlobSeqHelpers.h"
+
+//remove spaces
+std::string GlobSeqHelpers::chomp(std::string &str)
+{
+   str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
+   return str;
+}
+
---
+++ GlobSeqHelpers.h
@@ -0,0 +1,19 @@
+//
+//  GlobSeqHelpers.hpp
+//  All
+//
+//  Created by Cristian Andres Vogel on 01/07/2020.
+//
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+
+class GlobSeqHelpers
+{
+public:
+  
+  std::string chomp(std::string &str);
+  
+};
+
